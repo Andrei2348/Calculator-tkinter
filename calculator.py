cb3529f969225c756ba7af723779be64270b46fd
Andrei2348 <makarevichandco@gmail.com>
2023-09-01 12:28:25 +0300

    added styles

--- calculator.py
+++ calculator.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-
 root = Tk()
 root.resizable(width = False, height = False)
 root.geometry('330x456')
@@ -30,7 +29,7 @@ class CalculatorScreen():
                             fg = '#000',
                             disabledbackground = "#fff",
                             disabledforeground = "#000",
-                            font = 'Consolas 28',
+                            font = 'Arial 28',
                             state = DISABLED,
                             width = 32,
                             justify = RIGHT)
@@ -58,17 +57,26 @@ row = 1
 column = 0
 for index in btn_list:
   cmd = lambda x = index: output(x)
-  button = Button(master = frame_b, text = index, command = cmd, width = 5, height = 3, font = 'Arial 14', bg = 'yellow')
+  button = Button(master = frame_b, 
+                  text = index,
+                  command = cmd,
+                  width = 5,
+                  height = 3,
+                  font = 'Arial 14',
+                  bg = '#786b6b',
+                  activebackground="#918c8c",
+                  activeforeground="#000")
   button.grid(row = row, column = column, pady = 2, padx = 2)
   column += 1
   if column > 3:
     column = 0
     row += 1
   if index == '=':
-    button.configure(width = 13)
+    button.configure(width = 13, bg = '#c58424', activebackground="#cc9543", activeforeground="#000")
     button.grid(row=5, column=0, columnspan=2)
+              
   if index == 'C':
-    button.configure(width = 13)
+    button.configure(width = 13, bg = '#c58424', activebackground="#cc9543", activeforeground="#000")
     button.grid(row=5, column=2, columnspan=2)
 
 
@@ -76,7 +84,8 @@ def output(number):
   global string
   global dot_flag
   screen.clear()
-  
+
+  # Нажатие .
   if (number == '.'):
     if dot_flag == False:
       dot_flag = True
@@ -87,24 +96,29 @@ def output(number):
   else:
     string += number
     screen.write(string)
+
+  # Нажатие +-*/
   if number in elems:
     dot_flag = False
 
+  # Нажатие =
   if number == '=':
     string = str(eval(string[:-1]))
     screen.clear()
     screen.write(string)
-    # for element in string:
-    #   if element == '.':
+    dot_flag = False
+    for element in string:
+      if element == '.':
+        dot_flag = True
 
-
-
+  # Нажатие С
   if number == 'C':
     dot_flag = False
     string = ''
     screen.clear()
     screen.write('0')
 
+  # Нажатие СЕ
   if number == 'CE':
     screen.clear()
     if len(string) > 3:
@@ -118,6 +132,4 @@ def output(number):
       dot_flag = False
       screen.write('0')
     
-  
-  
 root.mainloop()
